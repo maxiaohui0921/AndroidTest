0df57d77aae1d3d6ea0d59e5c5401b0d005aaef1
maxiaohui0921 <38674823+maxiaohui0921@users.noreply.github.com>
2018-12-21 18:28:55 +0800

    12211828
    添加了抓取cpu memoryinfo等

--- adb/deviceLogger.py
+++ adb/deviceLogger.py
@@ -22,11 +22,55 @@ def getLogcat(deviceID,deviceName,keyword):
 def stopLogcat(pro):
     pro.terminate()
 
+#adb -s 192.168.29.248:5555 shell top -d 5 | grep -E 'beeboxes|opnext'
+def captureCPU(gap,pkg):
+    filename = config.log_path+"\\"+"cpu_"+fail_date+fail_time+".txt"
+    cpulog_file = open(filename, 'w')
+    cmd_cpu = "adb -s %s shell top -d %d | grep -E '%s'" % (config.deviceId,gap,pkg )
+
+    pro = subprocess.Popen(cmd_cpu, stdout=cpulog_file, stderr=subprocess.PIPE)
+    return pro,filename
+
+#获取内存信息
+#adb shell dumpsys meminfo | findstr "beeboxes opnext RAM"
+def captureMemory(gap,keyworks):
+    filename = config.log_path + "\\" + "memory_" + fail_date + fail_time + ".txt"
+    cpulog_file = open(filename, 'w')
+    cmd_meminfo = "adb -s %s shell dumpsys meminfo -d %d | grep -E '%s'" % (config.deviceId, gap, keyworks)
+    print(cmd_meminfo)
+    pro = subprocess.Popen(cmd_meminfo, stdout=cpulog_file, stderr=subprocess.PIPE)
+    return pro, filename
+
+#获取屏幕
+def captureScreen(filename):
+    cmd_capture = "adb -s %s shell screencap /sdcard/DCIM/%s.png" % (config.deviceId, filename+"_"+fail_date+"_"+fail_time)
+    #print(cmd_capture)
+    pic_name=filename+"_"+fail_date+"_"+fail_time
+    print("生成了错误图片："+pic_name+"  文件路径："+config.log_path)
+    subprocess.Popen(cmd_capture, shell=True)
+    time.sleep(3)
+    cmd_pull = "adb -s %s pull /sdcard/DCIM/%s.png %s" % (config.deviceId, pic_name,config.log_path)
+    #print("运行pull命令："+cmd_pull)
+    subprocess.Popen(cmd_pull, shell=True)
+    time.sleep(1)
+    cmd_rm = "adb -s %s shell rm /sdcard/DCIM/%s.png" % (config.deviceId, pic_name)
+    #print("运行rm命令："+cmd_rm)
+    subprocess.Popen(cmd_rm, shell=True)
+
 if __name__=="__main__":  #当前脚本运行实例
-    pro=getLogcat(config.deviceId,"desktop29",config.faceRecognizationKey)
+    # pro=getLogcat(config.deviceId,"desktop29",config.faceRecognizationKey)
+    # time.sleep(20)
+    # pro[0].terminate()
+    # print(pro[1])
+
+    # pro=captureCPU(5,"beeboxes|opnext")
+    # time.sleep(20)
+    # pro[0].terminate()
+    # print(pro[1])
+
+    # captureScreen("fr")
+
+    pro=captureMemory(5,"beeboxes|opnext|RAM")
     time.sleep(20)
     pro[0].terminate()
     print(pro[1])
-
-
-
--- utils/demo.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf-8-*-
-#__author__='maxiaohui'
-from uiautomator import Device
-
-deviceId="192.168.29.250:5555"
-d=Device(deviceId)
-d(resourceId="com.opnext.setting:id/ll_face_recognize").click()
