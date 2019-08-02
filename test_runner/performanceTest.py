53edd3e7cb0244fb570795589c9f1b2004259523
maxiaohui0921 <38674823+maxiaohui0921@users.noreply.github.com>
2019-08-02 20:59:13 +0800

    update

--- test_runner/performanceTest.py
+++ test_runner/performanceTest.py
@@ -9,7 +9,7 @@ from config import config
 import time
 
 def runFr1N():
-    print("请测试：在1：N模式下，连续刷脸")
+    print("请测试：在**模式下，连续刷脸")
     pro=deviceLogger.getLogcat(config.deviceId,"desktop29",config.faceRecognizationKey)
     time.sleep(65)
     pro[0].terminate()
@@ -17,7 +17,7 @@ def runFr1N():
     loggerHandler.analyzeLog(pro[1],config.oneByNPass)
 
 def runFr11():
-    print("请测试：在1：1模式下，连续刷脸")
+    print("请测试：在**模式下，连续刷脸")
     pro=deviceLogger.getLogcat(config.deviceId,"desktop29",config.faceRecognizationKey)
     time.sleep(65)
     pro[0].terminate()
@@ -25,7 +25,7 @@ def runFr11():
     loggerHandler.analyzeLog(pro[1],config.oneByonePass)
 
 def runImportLocal():
-    print("请测试：本地导入人员")
+    print("请测试：**1")
     pro = deviceLogger.getLogcat(config.deviceId, "desktop29", config.importLocalKey)
     time.sleep(1800)  #这里默认是半小时
     pro[0].terminate()
@@ -33,7 +33,7 @@ def runImportLocal():
     loggerHandler.analyzeLog(pro[1], config.importLocal)
 
 def runExportLocal():
-    print("请测试：本地导出人员")
+    print("请测试：**2")
     pro = deviceLogger.getLogcat(config.deviceId, "desktop29", config.exportLocalKey)
     time.sleep(1800)  # 这里默认是半小时,后期需要根据实际情况进行条件判断自动决定测试时间
     pro[0].terminate()
@@ -41,7 +41,7 @@ def runExportLocal():
     loggerHandler.analyzeLog(pro[1], config.exportLocal)
 
 def runSaasIssue():
-    print("请测试：Saas下发人员")
+    print("请测试：**3")
     pro = deviceLogger.getLogcat(config.deviceId, "desktop29", config.issueBySaasKey)
     time.sleep(5*60*60)  # 这里默认是4小时,后期需要根据实际情况进行条件判断自动决定测试时间
     pro[0].terminate()
