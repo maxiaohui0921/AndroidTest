9c15af623c05017fc1d712b14584edb66e48ea15
maxiaohui0921 <38674823+maxiaohui0921@users.noreply.github.com>
2019-08-06 14:31:51 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,23 +4,22 @@
 #device information
 deviceId="192.168.28.203:5555"
 devicePsw="2580"
-# deviceSN='FC241118370034'
-deviceSN='FC222218490092'
+deviceSN='FC2222'
 
 #log information
 log_path='F:\\log1'
 logKeyWord=''
 
 #adb keyword
-faceRecognizationKey="MatchProcessor"
-issueBySaasKey="PRETTY_LOGGER"
-importLocalKey='AddPeopleModel'
+faceRecognizationKey="MatchP"
+issueBySaasKey="PRETT"
+importLocalKey='AddPe'
 exportLocalKey=""    #还不是很清楚
 
 #event keywords  从log中分析数据时使用
-oneByonePass="match: success = 0"  #识别成功的次数    失败的次数不准确
+oneByonePass="match: = 0"  #识别成功的次数    失败的次数不准确
 oneByNPass="statusCode = 0"  #识别成功的次数
-saasIssueSuccess="add person, result: 0"
+saasIssueSuccess="add person: 0"
 importLocal="add people"
 exportLocal=""
 
@@ -38,8 +37,8 @@ oomKeyWord=['GC_FOR_ALLOC','GC_EXPLICIT','GC_CONCURRENT','GC_BEFORE_OOM']
 topTitle='PID USER PR NI CPU% S #THR VSS RSS PCY Name'
 
 #本地机器，识别的时候成功和失败的关键字  1:1 1:N快速和安全都没有关系，都有这个字段
-frPass='TERMINAL_VALIDATION_PASS'
-frFail='TERMINAL_VALIDATION_FAILURE'
+frPass='TERMINAL_PASS'
+frFail='TERMINAL_FAILURE'
 
 #webserver
 webserverHost="http://192.168.28.1:8080"
